d32b77824cc9b8f3c186e1d3b77903da3adc7040
Valentin Rogister <valentin.rogister@gmail.com>
2025-01-22 18:09:26 +0100

    day22

--- day22/part2.zig
+++ day22/part2.zig
@@ -1,92 +1,56 @@
 const std = @import("std");
 
 const Cube = struct {
-    on: bool,
+    sign: i8,
     p1: [3]i32,
     p2: [3]i32,
 
-    fn contains(self: *const @This(), p: [3]i32) bool {
+    fn intersection(self: *const @This(), other: @This()) ?@This() {
+        var p1: [3]i32 = undefined;
+        var p2: [3]i32 = undefined;
         for (0..3) |i| {
-            if (!((p[i] >= self.p1[i] and p[i] <= self.p2[i]) or
-                (p[i] >= self.p2[i] and p[i] <= self.p1[i]))) return false;
+            p1[i] = @max(self.p1[i], other.p1[i]);
+            p2[i] = @min(self.p2[i], other.p2[i]);
+            if (p1[i] > p2[i]) return null;
         }
-        return true;
+        return .{ .sign = 1, .p1 = p1, .p2 = p2 };
+    }
+
+    fn revertSign(self: *const @This()) @This() {
+        return .{ .sign = -1 * self.sign, .p1 = self.p1, .p2 = self.p2 };
+    }
+
+    fn volume(self: *const @This()) i64 {
+        var n: i64 = 1;
+        for (0..3) |i| n *= self.p2[i] - self.p1[i] + 1;
+        return self.sign * n;
     }
 };
 
-fn isOn(l: std.ArrayList(Cube), p: [3]i32) bool {
-    var on = false;
-    for (l.items) |cube| {
-        if (cube.contains(p)) on = cube.on;
-    }
-    return on;
-}
-
-fn compute(alloc: std.mem.Allocator, l: std.ArrayList(Cube)) !u64 {
-    const Lookup = struct {
-        alloc: std.mem.Allocator,
-        a: [3]std.ArrayList(i32),
-        l: std.ArrayList(Cube),
-
-        fn init(par: struct { alloc: std.mem.Allocator, l: std.ArrayList(Cube) }) !@This() {
-            var a: [3]std.ArrayList(i32) = undefined;
-            for (&a) |*ll| {
-                ll.* = std.ArrayList(i32).init(par.alloc);
-            }
-            for (par.l.items) |cube| {
-                for (&[_][3]i32{ cube.p1, cube.p2 }) |p| {
-                    for (0..3) |i| try a[i].append(p[i]);
-                }
-            }
-            for (&a) |*ll| {
-                std.mem.sort(i32, ll.items, {}, std.sort.asc(i32));
-            }
-            return .{ .alloc = par.alloc, .a = a, .l = par.l };
-        }
-
-        fn deinit(self: *@This()) void {
-            for (self.a) |ll| ll.deinit();
-        }
-
-        fn get_next_e(self: *const @This(), e: i32, i: usize) i32 {
-            var @"i1": usize = 0;
-            var @"i2": usize = self.a[i].items.len;
-            while (@"i2" - @"i1" > 1) {
-                const @"i3" = @divFloor(@"i2" + @"i1", 2);
-                if (self.a[i].items[@"i3"] == e) return e + 1;
-                if (self.a[i].items[@"i3"] > e) {
-                    @"i2" = @"i3";
+fn compute(alloc: std.mem.Allocator, l: std.ArrayList(Cube)) !i64 {
+    var cubes = std.ArrayList(Cube).init(alloc);
+    defer cubes.deinit();
+    for (l.items) |cube1| {
+        var new_cubes = std.ArrayList(Cube).init(alloc);
+        defer new_cubes.deinit();
+        for (cubes.items) |cube2| {
+            if (cube1.intersection(cube2)) |interCube| {
+                if (cube1.sign == 1 and cube2.sign == 1) {
+                    try new_cubes.append(interCube.revertSign());
+                } else if (cube1.sign == 1 and cube2.sign == -1) {
+                    try new_cubes.append(interCube);
+                } else if (cube1.sign == -1 and cube2.sign == 1) {
+                    try new_cubes.append(interCube.revertSign());
                 } else {
-                    @"i1" = @"i3";
+                    try new_cubes.append(interCube);
                 }
             }
-            return @max(self.a[i].items[@"i2"] - 1, e + 1);
         }
-    };
-    var lookup = try Lookup.init(.{ .alloc = alloc, .l = l });
-    defer lookup.deinit();
-    const a = lookup.a;
-    var n: u64 = 0;
-    var x = a[0].items[0];
-    while (x <= a[0].items[a[0].items.len - 1]) {
-        const nx = lookup.get_next_e(x, 0);
-        var nn: u64 = 0;
-        var y = a[1].items[0];
-        while (y <= a[1].items[a[1].items.len - 1]) {
-            const ny = lookup.get_next_e(y, 1);
-            var nnn: u64 = 0;
-            var z = a[2].items[0];
-            while (z <= a[2].items[a[2].items.len - 1]) {
-                const nz = lookup.get_next_e(z, 2);
-                if (isOn(l, [3]i32{ x, y, z })) nnn += @as(u64, @intCast(nz - z));
-                z = nz;
-            }
-            nn += @as(u64, @intCast(ny - y)) * nnn;
-            y = ny;
-        }
-        n += @as(u64, @intCast(nx - x)) * nn;
-        x = nx;
+        try cubes.appendSlice(new_cubes.items);
+        if (cube1.sign == 1) try cubes.append(cube1);
     }
+    var n: i64 = 0;
+    for (cubes.items) |cube| n += cube.volume();
     return n;
 }
 
@@ -97,7 +61,7 @@ pub fn solve(alloc: std.mem.Allocator, data: []const u8) !void {
         var it1 = std.mem.tokenizeScalar(u8, data, '\n');
         while (it1.next()) |line| {
             var cube: Cube = undefined;
-            cube.on = std.mem.startsWith(u8, line, "on");
+            cube.sign = if (std.mem.startsWith(u8, line, "on")) 1 else -1;
             const cleanLine = try alloc.dupe(u8, line);
             defer alloc.free(cleanLine);
             for (cleanLine) |*c| {
